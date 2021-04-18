df2cd9e85e9e73184514e696defb054209baad13
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-04-18 12:23:13 +0530

    Add files via upload

---
+++ ServerAppForQr/app/src/main/java/com/csociety/serverappforqr/Token.java
@@ -0,0 +1,17 @@
+package com.csociety.serverappforqr;
+public class Token {
+    public String uid,source,destination,tokens,amount,purchaseStatus,tokenid;
+    public Token(){
+
+    }
+
+    public Token(String uid, String tokenid,String source, String destination, String tokens, String amount,String purchaseStatus) {
+        this.uid = uid;
+        this.tokenid=tokenid;
+        this.source = source;
+        this.destination = destination;
+        this.tokens = tokens;
+        this.amount = amount;
+        this.purchaseStatus=purchaseStatus;
+    }
+}
